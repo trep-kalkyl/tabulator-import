0f60a9bcf8f9528cf05dc04c5845766dbdfb7812
trep-kalkyl <emil.appelkvist@gmail.com>
2025-02-04 09:25:12 +0100

    Update formatters.js

--- formatters.js
+++ formatters.js
@@ -1,1 +1,5 @@
-
+export function priceFormatter(cell) {
+    // Get the value from the cell and format it as currency
+    const value = cell.getValue();
+    return value ? value.toFixed(2) + " kr" : "-";
+}
