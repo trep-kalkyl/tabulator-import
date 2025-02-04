28b2efe1b9629826c498de8dfccbf6de8acbc646
trep-kalkyl <emil.appelkvist@gmail.com>
2025-02-04 09:27:06 +0100

    Update formatters.js

--- formatters.js
+++ formatters.js
@@ -1,5 +1,8 @@
+// Formatter function for prices
 export function priceFormatter(cell) {
-    // Get the value from the cell and format it as currency
+    // Get the value from the cell
     const value = cell.getValue();
+
+    // Format as currency (two decimal places) and add " kr"
     return value ? value.toFixed(2) + " kr" : "-";
 }
