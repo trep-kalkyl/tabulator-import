7254491b0705934dbbcf84ef0a596ed377230764
trep-kalkyl <emil.appelkvist@gmail.com>
2025-02-10 09:11:43 +0100

    Update calculation-engine.js

--- calculation-engine.js
+++ calculation-engine.js
@@ -1,60 +1,52 @@
-// Calculation engine for Tabulator
-
 class CalculationEngine {
-  constructor() {
-    this.tables = new Map();
-    this.dependencies = new Map();
-  }
-
-  // Register a table with the engine
-  registerTable(tableId, tabulatorInstance) {
-    this.tables.set(tableId, tabulatorInstance);
-  }
-
-  // Parse and evaluate expressions
-  evaluateExpression(expression, row, tableId) {
-    // Replace cell references with actual values
-    const parsedExpression = expression.replace(/([A-Z]+)(\d+)/g, (match, column, rowNum) => {
-      const targetTableId = tableId;
-      const targetRow = this.tables.get(targetTableId).getRow(rowNum);
-      return targetRow.getData()[column] || 0;
-    });
-
-    // Evaluate the expression
-    try {
-      return eval(parsedExpression);
-    } catch (error) {
-      console.error(`Error evaluating expression: ${expression}`, error);
-      return 0;
+    constructor() {
+        this.tables = new Map();
+        this.dependencies = new Map();
     }
-  }
 
-  // Update dependent cells
-  updateDependents(tableId, rowId, column) {
-    const key = `${tableId}:${rowId}:${column}`;
-    if (this.dependencies.has(key)) {
-      this.dependencies.get(key).forEach(dependent => {
-        const [depTableId, depRowId, depColumn] = dependent.split(':');
-        const table = this.tables.get(depTableId);
-        const row = table.getRow(depRowId);
-        row.update({ [depColumn]: this.evaluateExpression(table.getColumn(depColumn).definition.calculation, row, depTableId) });
-      });
+    registerTable(tableId, tabulatorInstance) {
+        this.tables.set(tableId, tabulatorInstance);
     }
-  }
 
-  // Set up dependencies
-  setupDependencies(tableId, calculation) {
-    const dependencies = calculation.match(/([A-Z]+)(\d+)/g) || [];
-    dependencies.forEach(dep => {
-      const [column, rowId] = dep.match(/([A-Z]+)(\d+)/).slice(1);
-      const key = `${tableId}:${rowId}:${column}`;
-      if (!this.dependencies.has(key)) {
-        this.dependencies.set(key, new Set());
-      }
-      this.dependencies.get(key).add(`${tableId}:${rowId}:${calculation}`);
-    });
-  }
+    evaluateExpression(expression, row, tableId) {
+        const data = row.getData();
+        console.log("Evaluating expression:", expression, "with data:", data);
+        const parsedExpression = expression.replace(/([A-Z]+)/g, (match, column) => {
+            return data[column] || 0;
+        });
+
+        try {
+            return eval(parsedExpression);
+        } catch (error) {
+            console.error(`Error evaluating expression: ${expression}`, error);
+            return 0;
+        }
+    }
+
+    updateDependents(tableId, rowId, column) {
+        const key = `${tableId}:${rowId}:${column}`;
+        if (this.dependencies.has(key)) {
+            this.dependencies.get(key).forEach(dependent => {
+                const [depTableId, depRowId, depColumn] = dependent.split(':');
+                const table = this.tables.get(depTableId);
+                const row = table.getRow(depRowId);
+                row.update({ [depColumn]: this.evaluateExpression(table.getColumn(depColumn).definition.calculation, row, depTableId) });
+            });
+        }
+    }
+
+    setupDependencies(tableId, calculation) {
+        const dependencies = calculation.match(/([A-Z]+)/g) || [];
+        dependencies.forEach(dep => {
+            const key = `${tableId}:${dep}`;
+            if (!this.dependencies.has(key)) {
+                this.dependencies.set(key, new Set());
+            }
+            this.dependencies.get(key).add(`${tableId}:${calculation}`);
+        });
+    }
 }
 
-// Export a single instance of the calculation engine
-export const calculationEngine = new CalculationEngine();
+const calculationEngine = new CalculationEngine();
+
+export { calculationEngine };
